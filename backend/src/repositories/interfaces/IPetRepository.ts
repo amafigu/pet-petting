c0176968f382a38200b0e3ced8f5ac5e2ab8e90f
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-17 20:03:02 +0100

    delete messages

--- backend/src/repositories/interfaces/IPetRepository.ts
+++ backend/src/repositories/interfaces/IPetRepository.ts
@@ -4,9 +4,6 @@ export interface IPetRepository {
   create(pet: Pet): Promise<void>
   findById(id: number): Promise<Pet | null>
   getAll(): Promise<Pet[]>
-  update(
-    id: number,
-    pet: Partial<Pet>
-  ): Promise<{ success: boolean; message?: string }>
-  delete(id: number): Promise<{ success: boolean; message?: string }>
+  update(id: number, pet: Partial<Pet>): Promise<void>
+  delete(id: number): Promise<void>
 }
