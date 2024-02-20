1832fd3e4c923e8ed3b325d706f75c57182f6d3f
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-02-20 01:06:09 +0100

    clean code

--- backend/src/repositories/interfaces/IPetRepository.ts
+++
@@ -1,9 +0,0 @@
-import { type Pet } from '../../entity/Pet'
-
-export interface IPetRepository {
-  create(pet: Pet): Promise<void>
-  findById(id: number): Promise<Pet | null>
-  getAll(): Promise<Pet[]>
-  update(id: number, pet: Partial<Pet>): Promise<void>
-  delete(id: number): Promise<void>
-}
