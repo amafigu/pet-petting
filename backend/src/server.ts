80dd71781f454d40e72cdbf1ddafb94d752d85e5
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-19 00:44:49 +0100

    export app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,3 +30,5 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at PORT: ${process.env.PORT}`)
 })
+
+export default app
